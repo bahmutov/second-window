c92b8d37480bc1502d8e9827090bb95ffb46dfa1
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2023-03-13 15:22:22 -0400

    working

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -3,7 +3,7 @@
 it('opens window in the current test runner', () => {
   cy.on('uncaught:exception', () => false)
   cy.visit('/browser-windows').then((win) => {
-    cy.stub(win, 'open').as('open')
+    cy.stub(win, 'open').returns({}).as('open')
   })
   cy.get('#messageWindowButton').click()
   cy.get('@open').should('have.been.calledWith', '', 'MsgWindow')
